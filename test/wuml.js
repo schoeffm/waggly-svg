1baeeab2779b3a6b939aa5c4d6a12fcb4689a3cb
Stefan Schöffmann <stefan.schoeffmann@posteo.de>
2015-05-14 16:15:01 +0200

    Added some tests and incremented the version string

--- test/wuml.js
+++ test/wuml.js
@@ -5,19 +5,42 @@ var exec = require('child_process').exec;
 var path = require('path');
 
 describe('wuml bin', function(){
-	var cmd = 'node '+path.join(__dirname, '../bin/wuml')+' ';
+	var cmd = 'node '+path.join(__dirname, '../bin/wsvg')+' ';
 	console.log(cmd);
-/*
+
 	it('--help should run without errors', function(done) {
 		exec(cmd+'--help', function (error, stdout, stderr) {
 			assert(!error);
+			assert(stdout.indexOf('Usage: wsvg') > 0);
 			done();
 		});
 	});
 
+	it('-i without parameter should exit with error', function(done) {
+		exec(cmd+'-i', function (error, stdout, stderr) {
+			assert(error);
+			done();
+		});
+	});
+
+	it('-i with nonexisting file should exit with error', function(done) {
+		exec(cmd+'-i foo.svg', function (error, stdout, stderr) {
+			assert(error);
+			done();
+		});
+	});
+
+	it('-i with existing file should just work', function(done) {
+		exec(cmd+'-i test/input.svg', function (error, stdout, stderr) {
+			assert(!error);
+			done();
+		});
+	});
+	
 	it('--version should run without errors', function(done) {
 		exec(cmd+'--version', function (error, stdout, stderr) {
 			assert(!error);
+			assert.strictEqual(stdout, '0.0.4\n');
 			done();
 		});
 	});
@@ -32,15 +55,4 @@ describe('wuml bin', function(){
 		});
 
 	});
-
-	it('should return error on unknown command', function(done) {
-        this.timeout(4000);
-
-		exec(cmd+'junkcmd', function (error, stdout, stderr) {
-			assert(error);
-			assert.equal(error.code,1);
-			done();
-		});
-	});
-*/
 });
